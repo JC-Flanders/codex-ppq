642f36caff9a4959b96f391d6799308aa8855192
dev <dev@srv1589336.hstgr.cloud>
2026-04-30 23:42:47 +0700

    added astro

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,8 +1,19 @@
 import { defineConfig } from "astro/config";
+import beautifier from "astro-html-beautifier";
 import viteConfig, { allowedHosts } from "./vite.config.js";
 
 export default defineConfig({
-  integrations: [],
+  compressHTML: false,
+  integrations: [
+    beautifier({
+      indent_size: 2,
+      indent_char: " ",
+      wrap_line_length: 0,
+      wrap_attributes: "auto",
+      preserve_newlines: false,
+      unformatted: ["astro-island"]
+    })
+  ],
   devToolbar: {
     enabled: false
   },
@@ -11,7 +22,8 @@ export default defineConfig({
     allowedHosts
   },
   build: {
-    format: "file"
+    format: "file",
+    inlineStylesheets: "never"
   },
   trailingSlash: "never",
   vite: viteConfig
